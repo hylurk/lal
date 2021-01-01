98c8578265fb36640b40b115ec0dd6515d52a7d9
hylurk <hylurk@163.com>
2021-01-01 22:35:05 +0800

    feat: onerror

--- koa2/src/application.js
+++ koa2/src/application.js
@@ -29,6 +29,9 @@ const http = require('http')
 // 继承 Node 的 events 模块的一些方法和属性
 const Emitter = require('events')
 
+// 使用 Node 的 util 模块
+const util = require('util')
+
 // 本身继承了 Node Events 模块的一些属性和方法
 module.exports = class Application extends Emitter {
   constructor (options = {}) {
@@ -54,7 +57,25 @@ module.exports = class Application extends Emitter {
   }
   // 返回一个 request 的回调函数，用来后面传给 http.createServer() 函数
   callback () {
+    // 使用 koa-compose 库，递归执行中间件函数，最终返回一个 Promise
     const fn = compose(this.middleware)
+    // 此处是用的继承的 Node Events 模块的方法
+    // this.listenCount 其作用就是正在监听的名为 eventName 的事件的监听器的数量
+    // 如果不了解，可以查看：http://nodejs.cn/api/events.html#events_emitter_listenercount_eventname
+    // this.on 其作用就是监听指定名为 eventName 的事件，并将回调函数放入监听器中
+    // 但不会去判断是否已经监听过，如果多次监听，那么就会多次添加
+    // 如果不了解，可以查看：http://nodejs.cn/api/events.html#events_emitter_off_eventname_listener
+    if (!this.listenCount('error')) this.on('error', this.onerror) // 如果没有监听过 error ，则调用 this.on 方法去监听
+    // 声明传递给 http.createServer 的回调函数
+    // 回调函数里，会被自动塞入两个参数，一个是 req 请求信息，一个是 res 响应信息
+    const handleRequest = (req, res) => {
+      // 调用自己的函数，封装出来一个上下文 ctx
+      // TODO
+      const ctx = this.createContent(req, res)
+      // TODO >>>
+      return this.handleRequest(ctx, fn)
+    }
+    return handleRequest
   }
   // 监听指定端口
   listen (...args) {
@@ -66,5 +87,25 @@ module.exports = class Application extends Emitter {
     // 启动一个 TCP 服务，并监听指定的端口，执行传入的回调函数
     // 如果不了解，可以查看：http://nodejs.cn/api/net.html#net_server_listen
     return server.listen(...args)
+  }
+  // 自定义处理错误的方法
+  onerror (err) {
+    // 判断是不是原生的错误
+    const isNativeError = Object.prototype.toString.call(err) === '[object error]' || err instanceof Error
+    // 如果不是原生的错误，则抛出异常
+    // 此处用到了 Node 的 util 模块 中的 format 方法
+    // 如果不了解，可以查看：http://nodejs.cn/api/util.html#util_util_format_format_args
+    // %j 意思为格式化为 json 字符串
+    if (!isNativeError) throw new TypeError(util.format('non-error thrown: %j', err))
+    // TODO 如果状态码为 404 或者>>>>>>
+    if (404 === err.status || err.expose) return
+    // TODO 如果是静默模式，则不打印错误>>>>>>
+    if (this.silent) return
+    // 拿到错误信息，Node 中的错误信息都是实例化或继承自原生 Error 类
+    // err.stack 表示被调用的 new Error() 在代码中的位置
+    // 如果 err 是一个对象，则调用其 toString 方法
+    const msg = err.stack || err.toString()
+    // 打印错误
+    console.error(`\n${msg.replace(/^/gm, '  ')}\n`)
   }
 }
